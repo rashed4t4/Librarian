8a016c8306982ebfe230986f934de1b10c7a0edf
rashed4t4 <34895170+rashed4t4@users.noreply.github.com>
2018-02-10 19:44:41 +0600

    Assignment-5

--- librarian.js
+++ librarian.js
@@ -3,35 +3,56 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
   /* Task 2 */
+  { title: "The Grapes of Wrath", year: "1939", author: "John Steinbeck", publisher: "Penguin Books" }
 ];
 
 /* Task 1 */
 function getBookTitle( index ){
-  //add code
+  return library[index].title;
 }
 
 console.log(getBookTitle(3));
 
 /* Task 3 */
 function addBook( book ){
-  //add code
+  library[library.length] = book;
+  return library;
 }
 
-var newBook = {};
+var newBook = {title: "To Kill a Mockingbird ", year: "1960", author: "Harper Lee", publisher: "Perennial Modern Classics"};
 console.log(addBook(newBook));
 
 /* Task 4 */
 function bookByAuthor(){
-  //add code
+  library.sort(function(a, b) {
+    var nameA = a.author.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.author.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+  return library;
 }
 
 console.log(bookByAuthor());
 
 /* Task 5 */
 function findByTitle( title ){
-  //add code
+  var result = [];
+  for(var i=0; i<library.length; i++){
+    if(library[i].title.toUpperCase().indexOf(title.toUpperCase()) >= 0){
+      result[result.length] = library[i];
+    }
+  }
+  return result;
 }
 
 console.log(findByTitle("man"));
